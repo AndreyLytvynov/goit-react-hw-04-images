79fc9f79298769649efa0b013506717dc020320a
AndreyLytvynov <mrjastonehome@gmail.com>
2022-11-26 14:24:00 +0200

    instal packs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import { nanoid } from 'nanoid';
-import ContactForm from './ContactForm/ContactForm';
-import Filter from './Filter/Filter';
-import ContactList from './ContactList/ContactList';
-import { Box } from './Box';
+// import { nanoid } from 'nanoid';
+// import { Box } from './Box';
 
 class App extends Component {
   state = {
@@ -12,8 +9,7 @@ class App extends Component {
   };
 
   render() {
-    const filterContacts = this.getFilterContacts();
-    return <div></div>;
+    return <div>Start</div>;
   }
 }
 
